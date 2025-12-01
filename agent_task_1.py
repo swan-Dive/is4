8b5de6380bba3d946cf6ee880a870fc1e962d133
swan-Dive <84986690+swan-Dive@users.noreply.github.com>
2025-12-01 20:21:27 +0300

    Update agent_task_1.py

--- agent_task_1.py
+++ agent_task_1.py
@@ -6,10 +6,51 @@ from pade.acl.messages import ACLMessage
 from pade.core.agent import Agent
 from pade.misc.utility import display_message, call_later, start_loop
 
-STARTED_AID = AID('starter@localhost:52003')
+fields = ['Теоретическая информатика', "Техническая информатика", 'Прикладная информатика', 'Информационные системы',
+          'Компьютерные сети и телекоммуникации', 'Базы данных', 'Информационная безопасность и кибербезопасность',
+          'Анализ данных и визуализация']
 
 MANAGER_AID = AID('manager@localhost:52000')
+TICKET_AID = AID('ticket@localhost:52001')
+QUESTION_AID = AID('question@localhost:52002')
+STARTED_AID = AID('starter@localhost:52003')
 
+class QuestionAgent(Agent):
+    def __init__(self, aid: AID):
+        super(QuestionAgent, self).__init__(aid)
+
+    def on_start(self):
+        super(QuestionAgent, self).on_start()
+        display_message(self.aid.localname, 'Question Agent started.')
+
+    def react(self, message):
+        super(QuestionAgent, self).react(message)
+
+
+class TicketAgent(Agent):
+    def __init__(self, aid: AID):
+        super(TicketAgent, self).__init__(aid)
+
+    def on_start(self):
+        super(TicketAgent, self).on_start()
+        display_message(self.aid.localname, 'Ticket Agent started.')
+
+    def react(self, message):
+        super(TicketAgent, self).react(message)
+
+
+class ManagerAgent(Agent):
+    def __init__(self, aid: AID):
+        super(ManagerAgent, self).__init__(aid)
+
+    def on_start(self):
+        super(ManagerAgent, self).on_start()
+        display_message(self.aid.localname, 'Manager Agent started.')
+
+    def react(self, message):
+        super(ManagerAgent, self).react(message)
+        if message.performative == ACLMessage.INFORM and message.sender == STARTED_AID:
+            display_message(self.aid.localname, 'Received message from starter')  
 
 class StarterAgent(Agent):
     def __init__(self, aid: AID):
@@ -31,9 +72,16 @@ class StarterAgent(Agent):
         self.send(message)
 
 if __name__ == '__main__':
+    questions = [{
+        "id": 'Qid_{}'.format(i),
+        "diff": random.randint(1, 5),
+        "field": secrets.choice(fields)
+    }for i in range(0, 100)]
 
+    q_agent = QuestionAgent(QUESTION_AID)
+    t_agent = TicketAgent(TICKET_AID)
 
-
+    m_agent = ManagerAgent(MANAGER_AID)
     s_agent = StarterAgent(STARTED_AID)
 
-    start_loop([s_agent])
+    start_loop([q_agent, t_agent, m_agent, s_agent])
