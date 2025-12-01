ebdd376d0dd6641b78f41fa5b16cb3a7763eae1f
swan-Dive <84986690+swan-Dive@users.noreply.github.com>
2025-12-01 20:17:58 +0300

    Update agent_task_1.py

--- agent_task_1.py
+++ agent_task_1.py
@@ -6,14 +6,9 @@ from pade.acl.messages import ACLMessage
 from pade.core.agent import Agent
 from pade.misc.utility import display_message, call_later, start_loop
 
-fields = ['Теоретическая информатика', "Техническая информатика", 'Прикладная информатика', 'Информационные системы',
-          'Компьютерные сети и телекоммуникации', 'Базы данных', 'Информационная безопасность и кибербезопасность',
-          'Анализ данных и визуализация']
-
-MANAGER_AID = AID('manager@localhost:52000')
-TICKET_AID = AID('ticket@localhost:52001')
-QUESTION_AID = AID('question@localhost:52002')
 STARTED_AID = AID('starter@localhost:52003')
+
+
 
 class StarterAgent(Agent):
     def __init__(self, aid: AID):
@@ -35,16 +30,9 @@ class StarterAgent(Agent):
         self.send(message)
 
 if __name__ == '__main__':
-    questions = [{
-        "id": 'Qid_{}'.format(i),
-        "diff": random.randint(1, 5),
-        "field": secrets.choice(fields)
-    }for i in range(0, 100)]
 
-    q_agent = QuestionAgent(QUESTION_AID)
-    t_agent = TicketAgent(TICKET_AID)
 
-    m_agent = ManagerAgent(MANAGER_AID)
+
     s_agent = StarterAgent(STARTED_AID)
-    
-    start_loop([q_agent, t_agent, m_agent, s_agent])
+
+    start_loop([s_agent])
