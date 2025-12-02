d44c9211cae9c5fa97ca8ce7c6384b9dd1629f65
swan-Dive <84986690+swan-Dive@users.noreply.github.com>
2025-12-02 08:17:44 +0300

    Update agent_task_1.py

--- agent_task_1.py
+++ agent_task_1.py
@@ -260,14 +260,14 @@ class ComportTemporal(TimedBehaviour):
 class StarterAgent(Agent):
     def __init__(self, aid: AID):
         super(StarterAgent, self).__init__(aid)
-        # comp_temp = ComportTemporal(self,  15.0, self.send_message)
-        # self.behaviours.append(comp_temp)
+        comp_temp = ComportTemporal(self,  15.0, self.send_message)
+        self.behaviours.append(comp_temp)
 
 
     def on_start(self):
         super(StarterAgent, self).on_start()
         display_message(self.aid.localname, 'Starter Agent started.')
-        call_later(4.0, self.send_message)
+        # call_later(4.0, self.send_message)
 
     def send_message(self):
         message = ACLMessage(ACLMessage.INFORM)
