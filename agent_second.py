d4b87370a7d3b5109e825daa3b610c953662e5f4
swan-Dive <84986690+swan-Dive@users.noreply.github.com>
2025-12-07 16:24:37 +0300

    Update agent_second.py

--- agent_second.py
+++ agent_second.py
@@ -43,7 +43,7 @@ class TicketAgent(Agent):
     def on_start(self):
         super(TicketAgent, self).on_start()
         display_message(self.aid.name, 'Ticket Agent started.')
-        self.call_later(10.0, self.send_message)
+        self.call_later(30.0, self.send_message)
 
     def react(self, message):
         super(TicketAgent, self).react(message)
@@ -75,21 +75,21 @@ class ManagerAgent(Agent):
     def react(self, message):
         super(ManagerAgent, self).react(message)
 
-        if message.performative == ACLMessage.INFORM and message.sender == STARTER_AID:
-            display_message(self.aid.localname, 'Received message from starter')
-            content = json.loads(message.content)
-            number_of_tickets = content.get('number_of_tickets', None)
-            number_of_questions = content.get('number_of_questions', None)
-
-            if number_of_tickets is not None and number_of_questions is not None:
-                self.number_of_tickets = number_of_tickets
-                self.number_of_questions = number_of_questions
-                self.react_create_ticket_list()
-
-
-
-    def react_create_ticket_list(self):
-        pass
+    #     if message.performative == ACLMessage.INFORM and message.sender == STARTER_AID:
+    #         display_message(self.aid.localname, 'Received message from starter')
+    #         content = json.loads(message.content)
+    #         number_of_tickets = content.get('number_of_tickets', None)
+    #         number_of_questions = content.get('number_of_questions', None)
+    # 
+    #         if number_of_tickets is not None and number_of_questions is not None:
+    #             self.number_of_tickets = number_of_tickets
+    #             self.number_of_questions = number_of_questions
+    #             self.react_create_ticket_list()
+    # 
+    # 
+    # 
+    # def react_create_ticket_list(self):
+    #     pass
 
 class ComportTemporal(TimedBehaviour):
     def __init__(self, agent, time, send_message):
@@ -112,7 +112,7 @@ class StarterAgent(Agent):
     def on_start(self):
         super(StarterAgent, self).on_start()
         display_message(self.aid.localname, 'Starter Agent started.')
-        call_later(10.0, self.send_message)
+        # call_later(10.0, self.send_message)
 
     def send_message(self):
         message = ACLMessage(ACLMessage.INFORM)
