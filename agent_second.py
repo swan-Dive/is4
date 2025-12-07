e30df8d0f0003409de5c428227d3b82dc1c39cdd
swan-Dive <84986690+swan-Dive@users.noreply.github.com>
2025-12-07 21:44:41 +0300

    Update agent_second.py

--- agent_second.py
+++ agent_second.py
@@ -177,8 +177,8 @@ class ManagerAgent(Agent):
         display_message(self.aid.localname, 'Manager Agent started.')
 
     def react(self, message):
-        super(ManagerAgent, self).react(message)
-
+        # super(ManagerAgent, self).react(message)
+        display_message(self.aid.name, message.decode())
         if message.performative == ACLMessage.INFORM and 'number_of_questions' in message.content:
             display_message(self.aid.localname, 'Received message from starter: {}'.format(message.content) )
             content = json.loads(message.content)
