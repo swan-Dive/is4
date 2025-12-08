09e92c8223a36aa28c70f4ea94c4e8b784bed038
swan-Dive <84986690+swan-Dive@users.noreply.github.com>
2025-12-08 19:39:01 +0300

    Update starter.py

--- starter.py
+++ starter.py
@@ -67,6 +67,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((this_host, this_port))
 s.listen(1)
 while True:
-    data = s.recv(1024)
-    print('Received', repr(data))
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            print(f'Received: {data}')
 s.close()
