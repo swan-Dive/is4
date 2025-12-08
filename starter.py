1c7bece5d638c657a9ef3066884de3b1db8d05c8
swan-Dive <84986690+swan-Dive@users.noreply.github.com>
2025-12-08 19:14:42 +0300

    Update starter.py

--- starter.py
+++ starter.py
@@ -54,9 +54,11 @@ s.connect((host, port))
 
 s.sendall(pickle.dumps('(inform '
           ':conversationID 4f1fce1c-d39a-11f0-a78f-525400150a7e'
-          ' :sender (agent-identifier :name starter@localhost:59000 :addresses (sequence localhost:59000))'
+          ' :sender (agent-identifier :name starter@155.212.171.31:59000 :addresses (sequence localhost:59000))'
           ' :receiver (set (agent-identifier :name manager@185.200.178.189:59001 :addresses (sequence manager@185.200.178.189:59001 ) )  )'
           ' :content "{"number_of_questions": 3, "number_of_tickets": 10}")'))
-# data = s.recv(1024)
+
 s.close()
-# print('Received', repr(data))
+s.connect((host, port))
+data = s.recv(1024)
+print('Received', repr(data))
